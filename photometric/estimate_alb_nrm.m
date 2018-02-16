2a7fd354b1837dba29dbd261a0856f2c9eb95c85
Nils Hulzebosch <macnilsmac@hotmail.com>
2018-02-16 18:28:56 +0100

    estimate albedo and norm

--- photometric/estimate_alb_nrm.m
+++ photometric/estimate_alb_nrm.m
@@ -19,6 +19,8 @@ end
 %   normal (3 channels)
 albedo = zeros(h, w, 1);
 normal = zeros(h, w, 3);
+%size(albedo)
+%size(normal)
 
 % =========================================================================
 % YOUR CODE GOES HERE
@@ -29,7 +31,25 @@ normal = zeros(h, w, 3);
 %   albedo at this point is |g|
 %   normal at this point is g / |g|
 
-
+for x = 1:1 % 512
+    for y = 1:1 % 512
+        i = reshape(image_stack(x, y, :), [5,1]);
+        scriptI = diag(i);
+        %size(scriptI)
+        %size(i)
+        %size(scriptV)
+        %test1 = scriptI * i
+        %test2 = scriptI * scriptV
+        %size(scriptI * i * ones(1,3))
+        %size(scriptI * scriptV)
+        g = (scriptI * i * ones(1,3)) / (scriptI * scriptV);
+        size(albedo(y, x, 1))
+        size(sum(g))
+        % TODO: FIX ERROR
+        albedo(y, x, 1) = sum(g);
+        normal(y, x, :) = g / sum(g);
+    end
+end
 
 % =========================================================================
 
