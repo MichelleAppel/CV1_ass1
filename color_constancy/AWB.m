9dacce5ffc34cea8d4247af5acabed66cbbf5c63
Nils Hulzebosch <macnilsmac@hotmail.com>
2018-02-21 17:23:20 +0100

    updated grey-world algorithm

--- color_constancy/AWB.m
+++ color_constancy/AWB.m
@@ -1,5 +1,6 @@
 % Color Constancy -- Grey-World Algorithm
 
+% clear figures and command window
 clear
 clc
 close all
@@ -15,27 +16,29 @@ R = awb_matrix(:, :, 1);
 G = awb_matrix(:, :, 2);
 B = awb_matrix(:, :, 3);
 
-% calculate mean of R, G, and B channel and final mean
+% calculate mean of R, G, and B channel
 R_mean = mean(mean(R));
 G_mean = mean(mean(G));
 B_mean = mean(mean(B));
-RGB_mean = mean([R_mean, G_mean, B_mean]);
 
 % calculate normalization factors
-R_norm = RGB_mean ./ R_mean;
-G_norm = RGB_mean ./ G_mean;
-K_norm = RGB_mean ./ B_mean;
+R_norm = 0.5 ./ R_mean;
+G_norm = 0.5 ./ G_mean;
+K_norm = 0.5 ./ B_mean;
 
 % normalize each color channel to create the corrected image
-corrected_img(:,:,1) = R_norm * double(R);
-corrected_img(:,:,2) = G_norm * double(G);
-corrected_img(:,:,3) = K_norm * double(B);
+corrected_img(:, :, 1) = R_norm * double(R);
+corrected_img(:, :, 2) = G_norm * double(G);
+corrected_img(:, :, 3) = K_norm * double(B);
+
+% save reconstructed image as .png
+%imwrite(corrected_img, 'norm_awb.jpg')
 
 % create the subplots
 figure;
 h    = [];
-h(1) = subplot(2,2,1);
-h(2) = subplot(2,2,2);
+h(1) = subplot(2, 2, 1);
+h(2) = subplot(2, 2, 2);
 
 % add images to figure
 image(awb_img, 'Parent', h(1));
